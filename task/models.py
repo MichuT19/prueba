122232c3996589a8ba82879e1fec3352026a4a7a
michel <atenecelai@uteq.edu.ec>
2023-08-08 14:11:20 -0500

    segundo

--- task/models.py
+++ task/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# Crea un nuevo usuario
+user = User.objects.create_user(username='nombre_de_usuario', password='contraseña')
+
 
 class task(models.Model):
     title = models.CharField(max_length=100)
